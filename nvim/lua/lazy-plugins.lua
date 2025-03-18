fb067dc7232ce5fb83ca85d65dc9c9432b4b5f8e
Leonhard Kost <leonhard.kost@gmail.com>
2025-03-18 21:50:52 +0100

    Update neovim config

---
+++ nvim/lua/lazy-plugins.lua
@@ -0,0 +1,38 @@
+require('lazy').setup({
+  'tpope/vim-sleuth',
+  require 'plugins.gitsigns',
+  require 'plugins.which-key',
+  require 'plugins.telescope',
+  require 'plugins.lspconfig',
+  require 'plugins.conform',
+  require 'plugins.cmp',
+  require 'plugins.tokyonight',
+  require 'plugins.todo-comments',
+  -- require 'plugins.mini',
+  require 'plugins.treesitter',
+  -- require 'plugins.debug',
+  require 'plugins.indent_line',
+  require 'plugins.lint',
+  require 'plugins.autopairs',
+  require 'plugins.neo-tree',
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
