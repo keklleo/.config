3b378adbf1a62042b270154cb32e37fbb80a33df
Leonhard Kost <leonhard.kost@gmail.com>
2025-06-01 11:54:15 +0200

    No greeting in shell

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,13 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 
 if status is-interactive
-    # vi key binds
-    set -g fish_key_bindings fish_vi_key_bindings
-    # accept completion with ctrl-y
-    bind --mode insert \cy forward-char
-    # aliases
-    test -e "$XDG_CONFIG_HOME/aliasrc" && \
-      source $XDG_CONFIG_HOME/aliasrc
+  # no greeting
+  set -g fish_greeting
+  # vi key binds
+  set -g fish_key_bindings fish_vi_key_bindings
+  # accept completion with ctrl-y
+  bind --mode insert \cy forward-char
+  # aliases
+  test -e "$XDG_CONFIG_HOME/aliasrc" && \
+    source $XDG_CONFIG_HOME/aliasrc
 end
