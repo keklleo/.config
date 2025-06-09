228a054915b4c820e14adf9f72cdbc3cf3ffc21c
Leonhard Kost <leonhard.kost@gmail.com>
2025-06-09 16:39:38 +0200

    Clean up diagnostic comments in neovim config

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -7,7 +7,6 @@ return {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
